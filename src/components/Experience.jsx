d79059a86326549037f3aa0f7ea69ed668188e51
J0key <shyraathaya@mail.ugm.ac.id>
2026-01-18 14:55:22 +0700

    first commit azure

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,22 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useThree } from "@react-three/fiber";
+import { TypingBox } from "./TypingBox";
 
 export const Experience = () => {
+  const texture = useTexture("textures/school.jpg");
+  const viewport = useThree((state) => state.viewport);
+
   return (
     <>
-      <OrbitControls />
+      <div className="z-10 md:justify-center fixed bottom-4 left-4 right-4 flex gap-3 flex-wrap justify-stretch">
+        <TypingBox />
+      </div>
+      <Avatar position={[0, -3, 5]} scale={2} />
+      <Environment preset="apartment" />
       <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+        <planeGeometry args={[viewport.width, viewport.height]} />
+        <meshBasicMaterial map={texture} />
       </mesh>
     </>
   );
